872d7f5dde35ac849859c9b6e46d53e6153166fc
Sxntiag0 <santiaguilar942@gmail.com>
2023-06-26 20:41:54 -0600

    subido

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBgWCiCGS-KbGHH7z1KW48NWtqkRkGrPSU",
+  authDomain: "proyectofinal-4403c.firebaseapp.com",
+  projectId: "proyectofinal-4403c",
+  storageBucket: "proyectofinal-4403c.appspot.com",
+  messagingSenderId: "33105810576",
+  appId: "1:33105810576:web:813ffe1cd269c2b152f415",
+  measurementId: "G-ZBN1HCLNW4"
+});
